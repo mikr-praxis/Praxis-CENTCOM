5f69c2e579af1f2fdd2e7237472d5457001ad93b
mikr-praxis <mscott@builtbypraxis.com>
2026-04-14 17:33:02 -0700

    refactor: simplify projects page server component

--- app/(app)/projects/page.tsx
+++ app/(app)/projects/page.tsx
@@ -1,9 +1,7 @@
-import { getProjects } from '@/actions/projects'
 import { ProjectsClient } from './projects-client'
 import { requireRole } from '@/lib/auth'
 
 export default async function ProjectsPage() {
   await requireRole('/projects')
-  const projects = await getProjects()
-  return <ProjectsClient initialProjects={projects} />
+  return <ProjectsClient />
 }
